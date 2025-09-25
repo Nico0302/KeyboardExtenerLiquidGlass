251e87bdd8be7a4b19efdcc57b5cfc866ea4eb04
Nicolas <nicolas@gres.io>
2025-09-25 20:58:48 +0200

    update example code

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,81 @@
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import {
+    Alert,
+    Keyboard,
     StyleSheet,
     Text,
     TextInput,
-    View
+    TouchableOpacity,
+    TouchableWithoutFeedback,
 } from "react-native";
 import {
-    KeyboardAwareScrollView,
-    KeyboardExtender
+    KeyboardExtender,
+    useKeyboardState,
 } from "react-native-keyboard-controller";
+import Reanimated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withTiming,
+} from "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function KeyboardExtendExample() {
+    const appearance = useKeyboardState((state) => state.appearance);
+    const [showExtend, setShowExtend] = useState(true);
+    const opacity = useSharedValue(1);
+
+    useEffect(() => {
+        opacity.set(withTiming(showExtend ? 1 : 0));
+    }, [showExtend]);
+
+    const animatedStyle = useAnimatedStyle(
+        () => ({
+            opacity: opacity.value,
+        }),
+        [],
+    );
+    const textStyle = useMemo(
+        () => [
+            styles.priceText,
+            appearance === "light"
+                ? styles.lightKeyboardText
+                : styles.darkKeyboardText,
+        ],
+        [appearance],
+    );
 
     return (
         <>
-            <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-                <TextInput
-                    placeholder="Donation amount"
-                    placeholderTextColor="#5c5c5c"
-                    style={styles.input}
-                />
-                <TextInput
-                    keyboardType="numeric"
-                    placeholder="Postal code"
-                    placeholderTextColor="#5c5c5c"
-                    style={styles.input}
-                />
-            </KeyboardAwareScrollView>
-            <KeyboardExtender enabled>
-                <View style={styles.keyboardExtend}>
-                    <Text style={styles.priceText}>10$</Text>
-                </View>
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <SafeAreaView edges={["top"]} style={styles.container}>
+                    <TextInput
+                        keyboardType="numeric"
+                        placeholder="Donation amount"
+                        placeholderTextColor="#5c5c5c"
+                        style={styles.input}
+                        onFocus={() => setShowExtend(true)}
+                    />
+                    <TextInput
+                        keyboardType="numeric"
+                        placeholder="Postal code"
+                        placeholderTextColor="#5c5c5c"
+                        style={styles.input}
+                        onFocus={() => setShowExtend(false)}
+                    />
+                </SafeAreaView>
+            </TouchableWithoutFeedback>
+            <KeyboardExtender enabled={showExtend}>
+                <Reanimated.View style={[styles.keyboardExtend, animatedStyle]}>
+                    <TouchableOpacity onPress={() => Alert.alert("10 dollars")}>
+                        <Text style={textStyle}>10$</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Alert.alert("20 dollars")}>
+                        <Text style={textStyle}>20$</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Alert.alert("50 dollars")}>
+                        <Text style={textStyle}>50$</Text>
+                    </TouchableOpacity>
+                </Reanimated.View>
             </KeyboardExtender>
         </>
     );
